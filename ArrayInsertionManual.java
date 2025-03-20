a50f29bdd867a70971a4096c64db7336c7b70633
M Mohammad Anas <mohammad.anas51@hotmail.com>
2025-03-20 14:41:23 +0530

    array insertion

--- ArrayInsertionManual.java
+++ ArrayInsertionManual.java
@@ -1,44 +1,64 @@
-import java.util.Scanner ;
+import java.util.Scanner;
+
 public class ArrayInsertionManual {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
+       
         System.out.println("Enter the size of the array");
         int n = scn.nextInt();
 
-        int[] arr = new int[n] ; 
+        int[] arr = new int[n];
 
+        
         System.out.println("Enter the array elements");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scn.nextInt();
         }
 
+        
         System.out.println("Enter the element to be inserted at the beginning");
         int firstElement = scn.nextInt();
-
 
         System.out.println("Enter the element to be inserted at the end");
         int lastElement = scn.nextInt();
 
-        // Create a new array with size increased by 2
-        int newSize = arr.length + 2;
+        
+        System.out.println("Enter the index where you want to insert a new element");
+        int specificIndex = scn.nextInt();
+
+        
+
+        System.out.println("Enter the element to insert at index " + specificIndex);
+        int specificElement = scn.nextInt();
+
+        
+        int newSize = arr.length + 3;
         int[] newArr = new int[newSize];
 
-        // Insert at the beginning
+       
         newArr[0] = firstElement;
 
-        // Copy old array to new array
-        for (int i = 0; i < arr.length; i++) {
-            newArr[i + 1] = arr[i]; // Shift right by 1
+        
+        for (int i = 0; i < specificIndex; i++) {
+            newArr[i + 1] = arr[i]; 
         }
 
-        // Insert at the end
-        newArr[newArr.length - 1] = lastElement;
+        
+        newArr[specificIndex + 1] = specificElement;
 
-        // Print updated array
+        
+        for (int i = specificIndex; i < arr.length; i++) {
+            newArr[i + 2] = arr[i]; 
+        }
+
+        
+        newArr[newSize - 1] = lastElement;
+
+        
         System.out.print("Updated Array: ");
-        for (int i = 0; i < newArr.length; i++) {
-            System.out.print(newArr[i] + " ");
+        for (int num : newArr) {
+            System.out.print(num + " ");
         }
 
         scn.close();
